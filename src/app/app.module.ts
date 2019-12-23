aadfe6c9b762ea52de2887324836d80d03f7d881
dhanush1993 <dhanush.srinivasa@gmail.com>
2019-12-23 04:44:11 -0800

    Updated Test page UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { SpotifyModule } from 'projects/sapi/src/public-api';
 import { LoginComponent } from './login/login.component';
 import { TestComponent } from './test/test.component';
+import { ServicesAvailableComponent } from './test/services-available/services-available.component';
+import { ResponseComponent } from './test/response/response.component';
+import { ServiceParamsComponent } from './test/service-params/service-params.component';
+import { TitlebarComponent } from './test/titlebar/titlebar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    TestComponent
+    TestComponent,
+    ServicesAvailableComponent,
+    ResponseComponent,
+    ServiceParamsComponent,
+    TitlebarComponent
   ],
   imports: [
     SpotifyModule,
-    BrowserModule
+    BrowserModule,
+    FormsModule, 
+    ReactiveFormsModule, 
   ],
   providers: [],
   bootstrap: [AppComponent]
